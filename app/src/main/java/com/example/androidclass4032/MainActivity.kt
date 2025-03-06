e010ef394969d0c89f7a354e6b7a3a876c9a453b
blueBye <navidshirmohammadi81@gmail.com>
2025-03-06 21:23:59 +0330

    top app bar and content

--- app/src/main/java/com/example/androidclass4032/MainActivity.kt
+++ app/src/main/java/com/example/androidclass4032/MainActivity.kt
@@ -4,31 +4,66 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.material3.CenterAlignedTopAppBar
-import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.androidclass4032.ui.theme.AppTheme
-
+import com.example.androidclass4032.ui.theme.onPrimaryContainerDark
+import com.example.androidclass4032.ui.theme.primaryContainerDark
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+//        enableEdgeToEdge()
         setContent {
             AppTheme {
-                CenterAlignedTopBar()
+                Scaffold(
+                    modifier = Modifier.fillMaxSize(),
+                    topBar = { Text("top bar") },
+                    content = { padding ->
+                        Greeting(name = "Android", modifier = Modifier.padding(padding))
+                    }
+                )
             }
         }
     }
 }
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun CenterAlignedTopBar() {
-    CenterAlignedTopAppBar(
-        title = {
-            Text(text = "Top bar")
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Column(
+        modifier=modifier.fillMaxWidth().background(color = primaryContainerDark).padding(8.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = "Hello $name!",
+            color = onPrimaryContainerDark,
+            modifier = modifier
+        )
+        Spacer(modifier = Modifier.padding(4.dp))
+        Button(onClick = {/* nothing */}) {
+            Text("Click Me")
         }
-    )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    AppTheme {
+        Greeting("Android")
+    }
 }
