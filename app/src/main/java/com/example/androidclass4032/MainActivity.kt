143cf51dc06213483c33acdab8a0362815c8200c
blueBye <navidshirmohammadi81@gmail.com>
2025-03-06 21:24:18 +0330

    top app bar and content (better approach)

--- app/src/main/java/com/example/androidclass4032/MainActivity.kt
+++ app/src/main/java/com/example/androidclass4032/MainActivity.kt
@@ -8,9 +8,12 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.statusBars
+import androidx.compose.foundation.layout.windowInsetsPadding
 import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -26,11 +29,11 @@ import com.example.androidclass4032.ui.theme.primaryContainerDark
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-//        enableEdgeToEdge()
+        enableEdgeToEdge()
         setContent {
             AppTheme {
                 Scaffold(
-                    modifier = Modifier.fillMaxSize(),
+                    modifier = Modifier.fillMaxSize().windowInsetsPadding(WindowInsets.statusBars),
                     topBar = { Text("top bar") },
                     content = { padding ->
                         Greeting(name = "Android", modifier = Modifier.padding(padding))
