86a82a2f6ac514c7f7b0ab229347f5bf42c21ec1
blueBye <navidshirmohammadi81@gmail.com>
2025-03-06 21:10:30 +0330

    used material theme

--- app/src/main/java/com/example/androidclass4032/MainActivity.kt
+++ app/src/main/java/com/example/androidclass4032/MainActivity.kt
@@ -4,21 +4,31 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.androidclass4032.ui.theme.AndroidClass4032Theme
+import androidx.compose.ui.unit.dp
+import com.example.androidclass4032.ui.theme.AppTheme
+import com.example.androidclass4032.ui.theme.onPrimaryContainerDark
+import com.example.androidclass4032.ui.theme.primaryContainerDark
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            AndroidClass4032Theme {
+            AppTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
                         name = "Android",
@@ -32,16 +42,27 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+    Column(
+        modifier=modifier.fillMaxWidth().background(color = primaryContainerDark).padding(8.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = "Hello $name!",
+            color = onPrimaryContainerDark,
+            modifier = modifier
+        )
+        Spacer(modifier = Modifier.padding(4.dp))
+        Button(onClick = {/* nothing */}) {
+            Text("Click Me")
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
-    AndroidClass4032Theme {
+    AppTheme {
         Greeting("Android")
     }
 }
